e0046d044fd8fce79b63ecf5aace46efaec07a4d
Luke Perkins <laperkin@ncsu.edu>
2018-08-09 10:36:07 -0400

    settings update for heroku

--- gis_app/settings.py
+++ gis_app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'z^52*at@7=%-d-4b1#gt6@2)dlg+!8#r%#i+as^%@*rht2#fx4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -42,7 +43,8 @@ INSTALLED_APPS = [
     'pandas',
     'geopandas',
     'folium',
-    'pyproj'
+    'pyproj',
+    'django_heroku'
 ]
 
 MIDDLEWARE = [
@@ -120,9 +122,21 @@ USE_L10N = True
 USE_TZ = True
 
 
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+    )
+
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+django_heroku.settings(locals())
+
+
+
